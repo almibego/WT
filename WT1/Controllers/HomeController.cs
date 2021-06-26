e22d9c3260e8ae84395969bea02a4d7500c2ffa2
Aleksei Mikhailov <aymikhailov@outlook.com>
2021-06-26 18:57:47 +0300

    Lab 2

--- WT1/Controllers/HomeController.cs
+++ WT1/Controllers/HomeController.cs
@@ -1,37 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using WT1.Models;
 
 namespace WT1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private List<ListDemo> _listDemo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            _listDemo = new List<ListDemo>
+                {
+                    new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+                    new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+                    new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+                };
         }
 
         public IActionResult Index()
         {
+            ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
             return View();
         }
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
     }
 }
